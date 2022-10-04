4d0a4ea3b89fc1732d14b4d136da11a91b9defe3
torbjkri <torbjorn.kringeland88@gmail.com>
2022-10-04 22:00:19 +0200

    Detection collision between player and enemy

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,13 +7,20 @@ public class EnemyBehaviour : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 3.0f;
     Rigidbody2D body;
+    Collider2D objectCollider;
+
     Vector2 lookDirection = new Vector2(1, 0);
     Rigidbody2D player;
+    Collider2D playerCollider;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Player")
-                    .GetComponent<Rigidbody2D>();
+        objectCollider = GetComponent<BoxCollider2D>();
+
+        GameObject playerObject = GameObject.Find("Player");
+        player =playerObject.GetComponent<Rigidbody2D>();
+        playerCollider = playerObject.GetComponent<BoxCollider2D>();
+
     }
 
     // Update is called once per frame
@@ -24,6 +31,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     void FixedUpdate()
     {
+        CheckCollisionWithPlayer();
         SetHeading();
         Vector2 position = body.position;
         Vector2 player_postion = player.position;
@@ -49,5 +57,13 @@ public class EnemyBehaviour : MonoBehaviour
 
         body.MoveRotation(rotation - angle);
         lookDirection = direction;
+    }
+
+    void CheckCollisionWithPlayer()
+    {
+        if (objectCollider.bounds.Intersects(playerCollider.bounds))
+        {
+            Debug.Log("Touching");
+        }
     }
 }
