68c4365deacf2e8304888c2a1f4c6ac4bd08ae87
sigbjorn313 <sklindnger@gmail.com>
2022-10-11 19:45:28 +0200

    Movement update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,32 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject projectilePrefab;
 
-    public float speed = 3.0f;
-    Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
+    public float moveSpeed = 5f;
+    Rigidbody2D rb;
+    public Camera cam;
+
     Vector2 lookDirection = new Vector2(1, 0);
     //public GameObject projectilePrefab;
-
+    Vector2 movement;
+    Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        Vector2 move = new Vector2(horizontal, vertical);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
 
         if (Input.GetKeyDown(KeyCode.C))
         {
@@ -39,20 +37,24 @@ public class PlayerController : MonoBehaviour
         }
 
     }
-    void FixedUpdate()
-    {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
 
-        rigidbody2d.MovePosition(position);
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
+
     void Launch()
     {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Vector2 lookDir = mousePos - rb.position;
+
+        GameObject projectileObject = Instantiate(projectilePrefab, rb.position  * 0.5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
+        projectile.Launch(rb.position, 300);
 
         //animator.SetTrigger("Launch");
     }
