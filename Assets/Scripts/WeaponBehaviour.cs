d61d39e8c111cec46189d17e4d9f0624187c1830
Tedrivs <larssmarkhus@gmail.com>
2023-04-11 21:09:25 +0200

    Added shotgun to weapon behaviour.

--- Assets/Scripts/WeaponBehaviour.cs
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class WeaponBehaviour : MonoBehaviour
 {
+    UpgradeSaveData playerUpgrades;
     [SerializeField] private GameObject bullet_type_;
     // Start is called before the first frame update
     void Start()
     {
+        playerUpgrades = SaveSystem.LoadUpgrades();    
         transform.localPosition = new Vector3(0.0f, 0.6f);
     }
 
@@ -21,5 +23,31 @@ public class WeaponBehaviour : MonoBehaviour
     {
         Debug.Log("ATTACKING");
         GameObject bullet = Instantiate(bullet_type_, transform.position, transform.rotation);
+        //Todo check what mode from upgrades
+
+        var mode = 1;
+        if((playerUpgrades != null && playerUpgrades.upgrades.Contains(5))) mode = 2;
+        switch (mode)
+        {
+            case 1:
+                Normal();
+                break;
+            case 2:
+                Shotgun();                
+                break;
+        }
+        
+    }
+
+    void Shotgun(){
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject bullet = Instantiate(bullet_type_, transform.position, transform.rotation);
+        }        
+    }
+
+    void Normal(){
+        GameObject bullet = Instantiate(bullet_type_, transform.position, transform.rotation);
+    
     }
 }
